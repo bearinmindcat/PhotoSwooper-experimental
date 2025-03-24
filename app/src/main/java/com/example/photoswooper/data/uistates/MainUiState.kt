1d0886423a7e32a7110ce3fa827d87eab13f0d11
loowiz <loowiz@envs.net>
2025-03-24 02:10:59 +0000

    feat: persistence - This adds a basic database which is updated with photos' status - Also improves performance by loading the first few photos before all have been loaded - This also changes how dates are stored to epoch milliseconds
    Signed-off-by: loowiz <loowiz@envs.net>

--- app/src/main/java/com/example/photoswooper/data/uistates/MainUiState.kt
+++ app/src/main/java/com/example/photoswooper/data/uistates/MainUiState.kt
@@ -4,7 +4,7 @@ import com.example.photoswooper.data.models.Photo
 import com.example.photoswooper.data.photoLimit
 
 data class MainUiState(
-    val photos: List<Photo> = listOf<Photo>(),
+    val photos: MutableList<Photo> = mutableListOf<Photo>(),
     val currentPhotoIndex: Int = 0,
     val numUnset: Int = photoLimit,
     val showReviewDialog: Boolean = false,
