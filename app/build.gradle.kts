c1fddc7f43dfebb5dc6321979541333b52ffc9ac
loowiz <loowiz@envs.net>
2025-07-28 01:49:56 +0100

    chore: bump versionCode & versionName again Signed-off-by: loowiz <loowiz@envs.net>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.photoswooper"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "2025.7.1"
+        versionCode = 4
+        versionName = "2025.7.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
