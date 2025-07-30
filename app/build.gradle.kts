900579aa6c0d39825efb3ed96fc2abbd0c403e47
loowiz <loowiz@envs.net>
2025-07-30 02:41:35 +0100

    chore: bump version to 2025.7.3 Signed-off-by: loowiz <loowiz@envs.net>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.example.photoswooper"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "2025.7.2"
+        versionCode = 5
+        versionName = "2025.7.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
